33bf98b2913783ceb55456dc6b1e5d5c3be9e06e
voncloft <voncloft@gmail.com>
2026-02-14 20:47:33 -0500

    Add lightbulb app icon and generic desktop launcher

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    const QIcon appIcon(QStringLiteral(":/assets/lightbulb.svg"));
+    a.setWindowIcon(appIcon);
     MainWindow w;
+    w.setWindowIcon(appIcon);
     w.show();
     return a.exec();
 }
