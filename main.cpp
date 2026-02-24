e5b36a38b56d801e5f437177ee72ad1fbed14892
voncloft <voncloft@gmail.com>
2026-02-24 04:32:59 -0500

    Fix KDE taskbar pinning for Lights app

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setApplicationName(QStringLiteral("lights"));
+    a.setDesktopFileName(QStringLiteral("lights"));
     const QIcon appIcon(QStringLiteral(":/assets/lightbulb.svg"));
     a.setWindowIcon(appIcon);
     MainWindow w;
